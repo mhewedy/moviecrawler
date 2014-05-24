5a80d5086347a775e2001a7b85e95c2d24b163c1
mhewedy <mohammed_a_hewedy@hotmail.com>
2014-05-24 12:42:00 +0300

    enhance the argument processing

--- src/main/java/mhewedy/App.java
+++ src/main/java/mhewedy/App.java
@@ -1,9 +1,22 @@
 package mhewedy;
 
+import mhewedy.beans.Movie;
+import mhewedy.crawler.InvalidCrawlerException;
 import mhewedy.crawler.WebsiteCrawler;
 
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.Set;
+
 /**
- * Hello world!
+ app <url>
+ app --hosts
+
+ app -v <url>
+ app <url> [limit]
+
+ app -v <url> [limit]
+
  */
 public class App {
     public static void main(String[] args) {
@@ -12,13 +25,22 @@ public class App {
             if ("--hosts".equals(args[0])) {
                 processShowHosts();
             } else {
-                processCrawler(args[0]);
+                processCrawler(args[0], -1);
             }
         } else if (args.length == 2) {
             if ("-v".equals(args[0])) {
                 Util.setVerbose(true);
-                processCrawler(args[1]);
-            } else {
+                processCrawler(args[1], -1);
+            } else if (args[1].matches("\\d+")){
+                processCrawler(args[0], Integer.parseInt(args[1]));
+            }else{
+                usage();
+            }
+        } else if (args.length == 3) {
+            if ("-v".equals(args[0]) && args[2].matches("\\d+")){
+                Util.setVerbose(true);
+                processCrawler(args[1], Integer.parseInt(args[2]));
+            }else{
                 usage();
             }
         } else {
@@ -26,16 +48,30 @@ public class App {
         }
     }
 
-    private static void processCrawler(String url) {
+    private static void processCrawler(String url, int limit) {
+        try {
+            WebsiteCrawler crawler = WebsiteCrawler.getCrawler(url);
+            Set<Movie> movies = crawler.getMovies(url, limit);
+
+            movies.stream()
+                    .sorted(Comparator.comparing(Movie::getRating).reversed())
+                    .forEach(m -> System.out.println(m.getName() + "\t" + m.getLink() + "\t" + m.getRating()));
+
+        } catch (InvalidCrawlerException | IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private static void processShowHosts() {
-        System.out.println("Following all available movie websites ");
+        System.out.println("Following all available movie websites: ");
         WebsiteCrawler.getAllSupportedDomains().forEach(System.out::println);
     }
 
     private static void usage() {
-        System.out.println("usage:\n\tapp <url>\n\tapp -v <url>\n\tapp --hosts");
+        String out = "usage:";
+        out += "\n\tapp --hosts";
+        out += "\n\tapp [-v] <url> [limit]";
+        System.out.println(out);
         System.exit(-1);
     }
 }
